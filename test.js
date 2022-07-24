d1653d9fa76af4b31983b3c9aeb5870c30cfc6d9
NhuTran1213 <95155265+NhuTran1213@users.noreply.github.com>
2022-07-24 16:10:36 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+function showTime() {
+    let time = newDate();
+    let year = time.getFullYear();
+    let month = time.getMonth();
+    let day = time.getDate();
+    let hour = time.getHours();
+    let mins = time.getMinutes();
+    let secs = time.getSeconds();
+}
